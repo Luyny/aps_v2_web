e621b68035a6a0d48ebda72467ed8452eca7158f
Luyny <l.f3rn4ndes@gmail.com>
2019-05-09 19:04:33 -0300

    Alterações

--- javascript.js
+++ javascript.js
@@ -1,25 +1,24 @@
-// let n = prompt()
-// let soma = 0
-// let lista = 0
-// for (let index = 0; index < n; index++) {
-//     lista = lista + 2
-//     soma = soma + lista
-//     document.write(lista+"<br>")
-// }
-// document.write("<br> SOMA =" + soma)
-// n(n+1)
-
-
-
-
+//Exercicio 1 - SOMA
+ let novaSoma = (n) => {
+    let soma=0
+    tabela.style.display = "table"
+    tabela.innerHTML = ` <tr class="thead-light "><th>N</th><th>Soma</th></tr>`
+    tabela.innerHTML += `<tr><td> <b>${n}</br> </td> <td> <b> ${n*(n+1)} </b> </td></tr>`
+    tabela.innerHTML += '<tr class="thead-light"><th colspan="2">Numeros pares</th></tr>'
+    for (let i = 0; i < n; i++) {
+        soma=soma+2
+    tabela.innerHTML += `<tr><td colspan="2">${soma}</td></tr>`
+    }
+}
+//Exercicio 2 - NOTAS
 let maior = undefined
 let menor = undefined
-let novaNota = function (nome,nota) {
-    tabela.style.display ="table"
+let novaNota = (nome, nota) => {
+    tabela.style.display = "table"
     maior_menor.style.display = "inline"
     tabela.innerHTML += `<tr><td>${nome}</td><td>${nota}</td></tr>`
     if (maior < nota || maior == undefined) maior = nota
     if (menor > nota || menor == undefined) menor = nota
-    nota_maior.innerHTML = `${maior}`
-    nota_menor.innerHTML = `${menor}`
+    nota_maior.innerHTML = maior
+    nota_menor.innerHTML = menor
 }
