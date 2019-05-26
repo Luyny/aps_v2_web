ad5df14f494182b22c5b322ecc2a431a26204a20
Luyny <l.f3rn4ndes@gmail.com>
2019-05-26 16:47:55 -0300

    Finalizado

--- javascript.js
+++ javascript.js
@@ -1,43 +1,65 @@
+// Função para postar o conteudo
+function postar(id, conteudo, adicionar) {
+    if (!adicionar) id.innerHTML = conteudo
+    if (adicionar) id.innerHTML += conteudo
+}
+// Função para indicar quando o valor inserido no campo esta incorreto
+// deve ser inserido apenas o id do campo e a mensagem que deve aparecer
+function valorIncorreto(id, mensagem) {
+    //Guarda o valor anterior do placeholder
+    let before = id.placeholder
+    //limpa o campo, exibe a msg de erro e adiciona a cor
+    id.value = ''
+    id.placeholder = mensagem
+    id.style.backgroundColor = '#ff00006e'
+    // Remove a cor e volta com o valor anterior depois de 1,3 segundos
+    setTimeout(() => {
+        id.style.background = ''
+        id.placeholder = before
+    }, 1300)
+}
 //Exercicio 1 - SOMA
 novaSoma = function (n) {
+    // Se o valor for negativo exibe msg de erro
     if (n < 0) {
-        alerta.innerHTML = ` 
-        <div class="ml-2 alert alert-danger float-left position-absolute w-25">        
-        Insira um número válido
-        </div>`
-        setTimeout(() => alerta.innerHTML = '', 2000)
+        valorIncorreto(numero, "Numero inválido")
     } else {
         let soma = 0
-        n != '' ? tabela.style.display = "table" : tabela.style.display = "none"
-        tabela.innerHTML = ` <tr class="thead-light "><th>N</th><th>Soma</th></tr>`
-        tabela.innerHTML += `<tr><td> <b>${n}</br> </td> <td> <b> ${n * (n + 1)} </b> </td></tr>`
-        tabela.innerHTML += '<tr class="thead-light"><th colspan="2">Numeros pares</th></tr>'
+        // se o campo de numero for vazio limpa a tabela 
+        if (n != '') {
+            tabela.style.display = "table"
+        } else {
+            tabela.style.display = "none"
+        }
+        // Forma a tabela
+        postar(tabela, `<tr class="thead-light "><th>N</th><th>Soma</th></tr>`, false)
+        postar(tabela, `<tr><td> <b>${n}</br> </td> <td> <b> ${n * (n + 1)} </b> </td></tr>
+                       <tr class="thead-light"><th colspan="2">Numeros pares</th></tr>`, true)
         for (let i = 0; i < n; i++) {
             soma = soma + 2
-            tabela.innerHTML += `<tr><td colspan="2">${soma}</td></tr>`
+            postar(tabela, `<tr><td colspan="2">${soma}</td></tr>`, true)
         }
     }
 }
 //Exercicio 2 - NOTAS
-let maior = undefined
-let menor = undefined
+var maior = undefined
+var menor = undefined
 let novaNota = function (nome, nota) {
-    if (nota == '' || nota < 0 || nota > 10 || nome == '') {
-        nomeForm.value = ''
-        notaForm.value = ''
-        alerta.innerHTML = ` 
-        <div class="ml-2 alert alert-danger float-left position-absolute w-25">        
-            Preencha corretamente todos os campos e tente novamente
-        </div>`
-        setTimeout(() => alerta.innerHTML = '', 2000)
-    }
-    else if (nome != '' && nota >= 0 && nota <= 10) {
+    //Verifica se o valor inserido é valido e chama a funcao caso nao seja
+    if (nome == '') valorIncorreto(nomeForm, "Nome Vazio")
+    if (nota < 0 || nota > 10) valorIncorreto(notaForm, "Nota inválida")
+    
+    if (nome != '' && nota >= 0 && nota <= 10) {
+        //Exibe a tabela que estava escondida
         tabela.style.display = "table"
         maior_menor.style.display = "inline"
-        tabela.innerHTML += `<tr><td>${nome}</td><td>${nota}</td></tr>`
+        postar(tabela, `<tr><td>${nome}</td><td>${nota}</td></tr>`, true)
+        //se for MAIOR que a nota armazenada como a MAIOR, substitui pela atual
         if (maior < nota || maior == undefined) maior = nota
+        //se for MENOR que a nota armazenada como a MENOR, substitui pela atual
         if (menor > nota || menor == undefined) menor = nota
-        nota_maior.innerHTML = maior
-        nota_menor.innerHTML = menor
+        // atualiza as notas maiores e menores
+        postar(nota_maior, maior, false)
+        postar(nota_menor, menor, false)
     }
 }
---
+++ navBar.js
@@ -0,0 +1,45 @@
+// Arquivo contendo a navBar do site
+
+// Determina qual a aba ativa pegando o valor colocado no html
+if (navbar.innerHTML == '1') {
+    var at1 = 'active border rounded'
+}else {
+    var at2 = 'active border rounded'
+}
+
+// NavBar
+navbar.innerHTML = `<nav id="nav" class="navbar navbar-dark navbar-expand-md mb-2 py-1 px-2">
+<a class="navbar-brand m-auto " href="index.html"><img src="/img/redentor1.png" width="125" height="30"></a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
+    aria-controls="navbarTogglerDemo" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarToggler">
+    <div class="navbar-text mr-auto pl-4 d-none d-lg-block"> APS 2 Projeto de Sistemas em Internet I</div>
+    <div class="nav-item dropdown m-auto">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button">Luyny Fernandes
+            Faria - 1801957</a>
+        <div class="dropdown-menu ml-5">
+            <a class="dropdown-item" target="_blank" href="https://instagram.com/luyny_f"><img
+                    src="/img/instagram.png" width="20"> Instagram</a>
+            <a class="dropdown-item" href="mailto:luynyfaria@gmail.com"><img src="/img/gmail.png" width="20"
+                    ></img>
+                E-mail</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" target="_blank" href="https://github.com/luyny"><img src="/img/github.png"
+                    width="20"></img> GitHub </a>
+        </div>
+    </div>
+    <div class="ml-auto">
+        <ul class=" navbar-nav">
+            <li class="nav-item ">
+                <a class="nav-link ${at1}" href="/index.html">Atividade 1</a>
+            </li>
+            <li class="nav-item ${at2}">
+                <a class="nav-link" href="/atividade2.html">Atividade 2</a>
+            </li>
+        </ul>
+    </div>
+</div>
+</nav>`
